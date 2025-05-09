c5bb92ce4de37c1a67a5e69fb76a71dd23e471b3
Sam Jones <captaisam7567@gmail.com>
2025-05-09 14:08:14 +0100

    Technically working, not run long enough to be able to know though. making shorter version

--- c++Test.cpp
+++ c++Test.cpp
@@ -4,16 +4,14 @@
 #include <array>
 #include <ctime>
 
-int i = 0;
 void RandomiseNumbers(std::vector<int>& Numbers, std::array<int, 10>& SelectedNumbers)
 {
-    i = 0;
+    int i = 0;
 
     while (!Numbers.empty())
     {
         int RandomInt = rand() % Numbers.size();
         int Selected = Numbers[RandomInt];
-        std::cout << Selected;
         SelectedNumbers[i] = Selected;
         i++;
 
@@ -21,13 +19,49 @@ void RandomiseNumbers(std::vector<int>& Numbers, std::array<int, 10>& SelectedNu
     }
 }
 
+bool IsSorted(std::array<int, 10>& SelectedNumbers)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        if (SelectedNumbers[i] > SelectedNumbers[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
     srand(static_cast<unsigned int>(time(0)));
 
-    std::vector<int> Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    std::vector<int> OriginalNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     std::array<int, 10> SelectedNumbers;
-    RandomiseNumbers(Numbers, SelectedNumbers);
+
+    while (true)
+    {
+        std::vector<int> Numbers = OriginalNumbers;
+        RandomiseNumbers(Numbers, SelectedNumbers);
+
+        if (IsSorted(SelectedNumbers))
+        {
+            for (int num : SelectedNumbers)
+            {
+                std::cout << num << " ";
+            }
+            std::cout << std::endl;
+            std::cout << "Sorted!" << std::endl;
+            exit(0);
+        }
+        else
+        {
+            for (int num : SelectedNumbers)
+            {
+                std::cout << num << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
 
     return 0;
 }
