c3887d5780a14f65ffb20d93448a28a580f1634f
Sam Jones <captaisam7567@gmail.com>
2025-05-09 13:35:01 +0100

    Correctly formatted everything

--- c++Test.cpp
+++ c++Test.cpp
@@ -1,26 +1,33 @@
+#include <random>
 #include <iostream>
 #include <vector>
-#include <random>
-#include <ctime>
 #include <array>
+#include <ctime>
 
 int i = 0;
-
-int main()
+void RandomiseNumbers(std::vector<int>& Numbers, std::array<int, 10>& SelectedNumbers)
 {
-    srand(static_cast<unsigned int>(time(0)));
-
-    std::vector<int> Numbers = { 1,2,3,4,5,6,7,8,9,10 };
-    std::array<int, 10> SelectedNumbers;
+    i = 0;
 
     while (!Numbers.empty())
     {
         int RandomInt = rand() % Numbers.size();
         int Selected = Numbers[RandomInt];
-        std::cout << Selected << "\n";
+        std::cout << Selected;
         SelectedNumbers[i] = Selected;
         i++;
 
         Numbers.erase(Numbers.begin() + RandomInt);
     }
+}
+
+int main()
+{
+    srand(static_cast<unsigned int>(time(0)));
+
+    std::vector<int> Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    std::array<int, 10> SelectedNumbers;
+    RandomiseNumbers(Numbers, SelectedNumbers);
+
+    return 0;
 }
